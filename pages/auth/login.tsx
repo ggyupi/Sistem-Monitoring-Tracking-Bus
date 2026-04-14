4716ffa5f4a1cac59d25f0529b9229ae76a4d304
Rizqi Bagus Andrean <bagus.ok55@gmail.com>
2026-04-14 20:34:55 +0700

    feat: update login callback URL and improve color scheme variables

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -46,7 +46,7 @@ export default function Login() {
       email: values.email,
       password: values.password,
       rememberMe: values.rememberMe,
-      callbackURL: "/",
+      callbackURL: "/user",
     });
 
     if (error) {
